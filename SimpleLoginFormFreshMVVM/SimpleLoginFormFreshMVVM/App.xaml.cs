1365be914adcd9e79feecf471ef02676d35dcf11
Bharat Kolla <bharatkolla11@gmail.com>
2022-01-27 20:37:27 -0600

    Add project files.

---
+++ SimpleLoginFormFreshMVVM/SimpleLoginFormFreshMVVM/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FreshMvvm;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleLoginFormFreshMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var mainPage = FreshPageModelResolver.ResolvePageModel<LoginPageModel>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SimpleLoginFormFreshMVVM/SimpleLoginFormFreshMVVM/MainPageModel.cs
@@ -0,0 +1,26 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLoginFormFreshMVVM
+{
+    internal class MainPageModel : FreshBasePageModel
+    {
+        public MainPageModel()
+        {
+
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+
+        public override void ReverseInit(object returnedData)
+        {
+            base.ReverseInit(returnedData);
+        }
+
+    }
+}
